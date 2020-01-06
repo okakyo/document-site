d93c04bc0cf00e38ff7aa2e41d552bc5a8004755
okakyo <0622okakyo@gmail.com>
2020-01-06 19:10:21 +0900

    FirstCommit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Django-site 
+
+## このアプリについて
+- このアプリは、Django を利用したアプリです。
+- 今回は, Vue や React といったフロントエンドフレームワークを使わずに構築することを前提としている
+
+##　ライブラリ
+- Django
+- Requests
+
+## ディレクトリ説明:
+- projects:
+    - projects:
+    - works:
+    - 
+
---
+++ api/works/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def signup():
+    return render()
+
+def signin():
+    return render()
+
+def index():
+    return render()
+
+def documentsFiles():
+    return render()
