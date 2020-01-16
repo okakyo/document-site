6167b127d188dd43d850c99a5712f92bef6f57d5
okakyo <0622okakyo@gmail.com>
2020-01-16 12:22:01 +0900

    修正

---
+++ api/works/learn.py
@@ -0,0 +1,6 @@
+# FTP サーバーから pickle ファイルや 学習データをデプロイできるようにする
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn import *
--- api/works/views.py
+++ api/works/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render,HttpResponse,
-
+from django.shortcuts import render,HttpResponse,redirect
+from linebot import WebhookHandler
 
 # キャッチアップ用のHTML
 def index(request):
     return render(request,'index.html')
 
-# 以下、ボットのシステムをc構築 
+# 以下、ボットのシステムを構築 
+
+def callback(request):
+    if(method=="POST"):
+        signature =  request.headers['X-Line-Signature']
+
+    else:
+        redirect('/')
+        
+
 def sayHello(request):
+    if(request.method== "POST"):
+        pass
     return json()
 
 def sendCode(request):
